06b5c4e94dec9eba54f3a02aaaa86588fb0239de
RaY8118 <parthghadi43@gmail.com>
2025-02-08 21:04:36 +0530

    Add a basic layout for chatroom

--- app/(tabs)/cgchat.jsx
+++ app/(tabs)/cgchat.jsx
@@ -1,12 +1,82 @@
-import { View, Text } from "react-native";
-import React from "react";
-
+import { View, Text } from 'react-native'
+import React, { useCallback, useEffect, useState } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
 const CgChat = () => {
-  return (
-    <View>
-      <Text>CgChat</Text>
-    </View>
-  );
-};
 
-export default CgChat;
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'This is a quick reply. Do you love Gifted Chat? (radio) KEEP IT',
+        createdAt: new Date(),
+        quickReplies: {
+          type: 'radio', // or 'checkbox',
+          keepIt: true,
+          values: [
+            {
+              title: '😋 Yes',
+              value: 'yes',
+            },
+            {
+              title: '📷 Yes, let me show you with a picture!',
+              value: 'yes_picture',
+            },
+            {
+              title: '😞 Nope. What?',
+              value: 'no',
+            },
+          ],
+        },
+        user: {
+          _id: 2,
+          name: 'React Native',
+        },
+      },
+      {
+        _id: 2,
+        text: 'This is a quick reply. Do you love Gifted Chat? (checkbox)',
+        createdAt: new Date(),
+        quickReplies: {
+          type: 'checkbox', // or 'radio',
+          values: [
+            {
+              title: 'Yes',
+              value: 'yes',
+            },
+            {
+              title: 'Yes, let me show you with a picture!',
+              value: 'yes_picture',
+            },
+            {
+              title: 'Nope. What?',
+              value: 'no',
+            },
+          ],
+        },
+        user: {
+          _id: 2,
+          name: 'React Native',
+        },
+      }
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+  return (
+    <View className='h-full bg-custom-white'>
+      <Text>Chat</Text>
+      <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1
+        }} />
+    </View>
+  )
+}
+
+export default CgChat
